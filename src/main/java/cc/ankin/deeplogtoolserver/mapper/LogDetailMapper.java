ef5506140ff2e284bf3dff1ff70226b54d94d34e
Yiqun Zhang <u7102332@anu.edu.au>
2021-08-30 01:58:33 +0800

    init

---
+++ src/main/java/cc/ankin/deeplogtoolserver/mapper/LogDetailMapper.java
@@ -0,0 +1,7 @@
+package cc.ankin.deeplogtoolserver.mapper;
+
+import cc.ankin.deeplogtoolserver.pojo.LogDetail;
+
+public interface LogDetailMapper {
+    void insertLogDetail(LogDetail logDetail);
+}
